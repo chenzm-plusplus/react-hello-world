6e0c93cdf4602f6ef78ad371ee271960e4767ea1
陈张萌 <2017013678@secoder.net>
2021-07-04 15:00:43 +0800

    [react] test: 9chess

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function App2() {
+  return (
+    <div className="App2">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link2"
+          href="https://www.baidu.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello World
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// export default App;
+export default App2;
